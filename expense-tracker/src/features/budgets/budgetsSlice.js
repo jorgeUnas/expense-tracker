934f685f099c04d4c6791a45487e1c5830c94f6a
EC2 Default User <ec2-user@ip-172-31-28-134.eu-north-1.compute.internal>
2024-04-10 15:25:18 +0000

    Creating all the files for the expense tracker app

---
+++ expense-tracker/src/features/budgets/budgetsSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CATEGORIES = [
+  "housing",
+  "food",
+  "transportation",
+  "utilities",
+  "clothing",
+  "healthcare",
+  "personal",
+  "education",
+  "entertainment",
+];
+const initialState = CATEGORIES.map((category) => ({
+  category: category,
+  amount: 0,
+}));
+
+const budgetsSlice = createSlice();
+
+export const editBudget = (budget) => {
+  return {
+    type: "budgets/editBudget",
+    payload: budget,
+  };
+};
+
+const budgetsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "budgets/editBudget":
+      const newBudgets = state.map((budget) => {
+        if (budget.category === action.payload.category) {
+          return action.payload;
+        }
+        return budget;
+      });
+      return newBudgets;
+    default:
+      return state;
+  }
+};
+
+export const selectBudgets = (state) => state.budgets;
+export default budgetsReducer;
